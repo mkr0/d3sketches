fd7ef8af8297eac9e015165bfc09378b4581a0b2
mkrof <mkrof@users.noreply.github.com>
2015-09-22 20:12:52 -0400

    Test.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
-console.log('hello. asdf');
+import d3 from 'd3';
+
+const data = [25, 30, 58, 10, 43];
+
+d3.select('body')
+  .selectAll('div')
+  .data(data)
+  .enter()
+  .append('div')
+  .style('width', '25px')
+  .style('height', (d) => {return d + 'px';})
+  .style('display', 'inline-block')
+  .style('background', 'cyan');
